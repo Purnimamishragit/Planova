37ce87b60e8917490bd449eba2cd0ef94eba8fb3
Purnimamishragit <purnimam250@gmail.com>
2025-07-03 00:08:44 +0530

    Purnima

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value"; // Make sure this exists
 import Contact from "./components/Contacts/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Residencies/>
       <Value/>
       <Contact/>
+      <GetStarted/>
     </div>
   );
 }
