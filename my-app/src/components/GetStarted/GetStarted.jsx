80450468f5100038a6c185ba03652c09163d4e32
Purnimamishragit <purnimam250@gmail.com>
2025-07-06 23:48:00 +0530

    Purnima

--- my-app/src/components/GetStarted/GetStarted.jsx
+++ my-app/src/components/GetStarted/GetStarted.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import './GetStarted.css'
 const GetStarted = () => {
     return(
-        <div>Header</div>
+        <section className="g-wrapper">
+            <div className="paddings innerwidth g-container">
+                <div className="flexColCenter inner-container">
+                    <span className="primaryText">Get Started With Planova</span>
+                    <span className="secondaryText">Subscribe and find super attractive price quotes <br/>
+                    Find your residence soon
+                    </span>
+                    <button className="button">
+                        <a href="mailto:purnimam250@gmail.com">Get Started</a>
+                    </button>
+                </div>
+            </div>
+        </section>
     )
 }
 export default GetStarted
--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,23 +1,83 @@
-import React from 'react'
-import './Header.css'
+// import React, { useState } from "react";
+// import "./Header.css";
+// import { BiMenuAltRight } from "react-icons/bi";
+// const Header = () => {
+//     const [menuOpened,setMenuOpened]= useState(false)
+
+//     const getMenuStyles = (menuOpened)=>{
+//         if (document.documentElement.clientWidth<=800)
+//         {
+//             return {right: !menuOpened && "-100"}
+//         }
+//     }
+//   return (
+//     <section className="h-wrapper">
+//       <div className="flexCenter paddings innerWidth  h-container">
+//         <img src="./logo.png" alt="logo" width={100} />
+//         <div className="flexCenter h-menu">
+//             style={getMenuStyles(menuOpened)}
+//         </div>
+
+//           <a href="">Residencies</a>
+//           <a href="">Our Values</a>
+//           <a href="">Contact Us</a>
+//           <a href="">Get Started</a>
+//           <button className="button">
+//             <a href="">Contact</a>
+//           </button>
+//           <div className="cmenu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
+//             <BiMenuAltRight size={30} />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+// export default Header;
+
+import React, { useState } from "react";
+import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
+
 const Header = () => {
-    return(
-        <section className="h-wrapper">
-            <div className="flexCenter paddings innerWidth  h-container">
-                <img src="./logo.png" alt="logo" width={100}/>
-                <div className="flexCenter h-menu">
-                    <a href="">Residencies</a>
-                    <a href="">Our Values</a>
-                    <a href="">Contact Us</a>
-                    <a href="">Get Started</a>
-                    <button className="button">
-                        <a href="">Contact</a>
-                    </button>
-                    <a href=""></a>
-                    <a href=""></a>
-                </div>
-            </div>
-        </section>
-    )
-}
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  // Function to calculate dynamic styles for the menu based on viewport width
+  const getMenuStyles = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: menuOpened ? "0" : "-100px" }; // Adjusted for correct CSS syntax
+    }
+    return {}; // Return empty object if condition is not met
+  };
+
+  return (
+    <section className="h-wrapper">
+      <div className="flexCenter paddings innerWidth h-container">
+        <img src="./logo.png" alt="logo" width={100} />
+
+        <OutsideClickHandler
+        onOutsideClick={() => setMenuOpened(false)}
+        >
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+            <a href="">Residencies</a>
+            <a href="">Our Values</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Header;
